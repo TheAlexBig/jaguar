264ecfe971e7c4c2b2f298497734a004b75cb98b
AlexBig <josealexandergarciavalladares@gmail.com>
2023-06-04 16:08:25 -0600

    Use tmp for new feature

--- scripts/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-# Substitute environment variables in nginx.conf.template
-envsubst '${BACKEND_PORT},${FRONTEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Execute the default entrypoint script of the nginx image
-exec "$@"
