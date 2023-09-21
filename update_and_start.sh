14dd180b2a802a87667ca7a9fb34ed52cd40d8df
@jose.valladares <jvalladares@applaudostudios.com>
2023-09-21 10:14:34 -0600

    clean the place

--- update_and_start.sh
+++ update_and_start.sh
@@ -9,5 +9,8 @@ git submodule update --remote --merge
 # Stop Docker Compose services
 sudo docker-compose stop
 
+# Clean up Docker resources
+sudo docker system prune -f
+
 # Start Docker Compose with cleanup and build
 sudo docker-compose up --remove-orphans --build
